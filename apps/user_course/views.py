fb7516f9f6fde59dc7c427866fb5d027749c2ba4
Robert Tichy <robtichy@AOL.com>
2016-11-22 08:10:09 -0500

    refinements

--- apps/user_course/views.py
+++ apps/user_course/views.py
@@ -6,17 +6,26 @@ def index(request):
 
     if not request.session['user_id']:
         return redirect('/home/')
-    qcourses = models.Course.objects.all().order_by('title')
-    course_data = []
+    qcourses = models.CourseExtended.objects.all().order_by('course__title')
+    course_data=[]
     for each in qcourses:
-        eachX = models.CourseExtended.objects.get(course_id=each.id)
         ucount = 0
-        for user in each.user.all():
+        for user in each.course.user.all():
             ucount += 1
-        course_data.append({'students': ucount, 'description': eachX.description,
-        'id':each.id, 'title':each.title,
+        course_data.append({'students':ucount, 'description': each.description,
+        'id': each.course.id, 'title':each.course.title
          })
-
+    # qcourses = models.Course.objects.all().order_by('title')
+    # course_data = []
+    # for each in qcourses:
+    #     eachX = models.CourseExtended.objects.get(course_id=each.id)
+    #     ucount = 0
+    #     for user in each.user.all():
+    #         ucount += 1
+    #     course_data.append({'students': ucount, 'description': eachX.description,
+    #     'id':each.id, 'title':each.title,
+    #      })
+    #
     context={'users': models.User.objects.all(),
              'courses': qcourses,
              'user' : models.User.objects.get(id = request.session['user_id']),
